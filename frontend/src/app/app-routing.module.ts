bb4b46035a8daf6ca54f1fc5fe0cd54462245bc7
Gabriel Heinzer <dev@gabrielheinzer.ch>
2023-01-14 19:12:42 +0100

    Added filesa

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './pageComponents/home/home.component';
+import { NotFoundComponent } from './pageComponents/not-found/not-found.component';
+import { LoginComponent } from './pageComponents/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './header/header.component';
+import { LinkComponent } from './link/link.component';
+import { HomeComponent } from './pageComponents/home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './pageComponents/not-found/not-found.component';
+import { LoginComponent } from './pageComponents/login/login.component';
+import { InputComponent } from './input/input.component';
+import { IconComponent } from './icon/icon.component';
+
+@NgModule({
+  declarations: [AppComponent, LogoComponent, HeaderComponent, LinkComponent, HomeComponent, FooterComponent, NotFoundComponent, LoginComponent, InputComponent, IconComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
