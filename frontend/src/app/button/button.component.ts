cdbf7da857e2300e84bbfd2fa0938b91bf07df82
Gabriel Heinzer <dev@gabrielheinzer.ch>
2023-01-24 18:31:36 +0100

    ✨ Added functionality to button disable input

--- frontend/src/app/button/button.component.ts
+++ frontend/src/app/button/button.component.ts
@@ -1,12 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  HostBinding,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnChanges {
   @Input() icon: string = '';
   @Input() fill: 'background' | 'accent' = 'accent';
-  @Input() disabled: boolean = true;
+  @Input() disabled: boolean = false;
+  @Output() click = new EventEmitter<void>();
+
+  @HostBinding('style.pointerEvents') pointerEvents: string = 'initial';
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    this.pointerEvents = this.disabled ? 'none' : 'initial';
+  }
+
+  public clickHandler(ev: Event) {
+    if (!this.disabled) this.click.emit();
+  }
 }
