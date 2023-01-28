345cc0ecb490fab6e62f35722c2922aa0a0db38e
Gabriel Heinzer <dev@gabrielheinzer.ch>
2023-01-28 13:38:16 +0100

    💄 Changed loader to only span 90 deg

--- frontend/src/app/loader/loader.component.ts
+++ frontend/src/app/loader/loader.component.ts
@@ -8,12 +8,10 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
 export class LoaderComponent implements OnInit {
   @Input() color: string | null = null;
   @HostBinding('style.border-top-color') borderTop!: string;
-  @HostBinding('style.border-right-color') borderRight!: string;
 
   ngOnInit() {
     if (this.color !== null) {
       this.borderTop = this.color;
-      this.borderRight = this.color;
     }
   }
 }
