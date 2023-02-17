973453d504ab5c1f2050099274afe25e273fba63
Gabriel Heinzer <dev@gabrielheinzer.ch>
2023-02-17 14:36:57 +0100

    🧪 Extended the header spec

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -18,4 +18,49 @@ describe('Test the header', () => {
             .should('contain.html', 'app-input')
             .should('contain.text', 'admin');
     });
+
+    describe('Test user badge navigation', () => {
+        beforeEach(() => {
+            SINVCypressLib.openSession();
+            cy.get('app-user-badge .badge-menu').as('userBadgeMenu');
+        });
+
+        it('Test if the menu opens', () => {
+            cy.get('app-user-badge .badge-menu').should('not.be.visible');
+            cy.get('app-user-badge')
+                .click()
+                .get('.badge-menu')
+                .should('be.visible');
+
+            cy.get('@userBadgeMenu').contains('Settings').click();
+        });
+
+        it('Test settings link', () => {
+            cy.get('@userBadgeMenu')
+                .contains('Settings')
+                .click({ force: true });
+            cy.url().should('contain', 'settings');
+        });
+
+        it('Test repo settings link', () => {
+            cy.get('@userBadgeMenu')
+                .contains('Repository settings')
+                .click({ force: true });
+            cy.url().should('contain', 'repo-settings');
+        });
+
+        it('Test admin settings link', () => {
+            cy.get('@userBadgeMenu')
+                .contains('Admin settings')
+                .click({ force: true });
+            cy.url().should('contain', 'admin-settings');
+        });
+
+        it('Test logout', () => {
+            cy.get('@userBadgeMenu')
+                .contains('Sign out')
+                .click({ force: true });
+            cy.url().should('contain', 'login');
+        });
+    });
 });
